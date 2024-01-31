d8f9da94de0942fcd951695c5304dea1b37a7111
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-01-31 01:58:27 -0800

    Update index.js

--- pose-detection/demos/upload_video/backend/index.js
+++ pose-detection/demos/upload_video/backend/index.js
@@ -134,7 +134,7 @@ async function compareAngles(user_angles) {
     const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: messages,
-        stream: true,
+        // stream: true,
     });
     // const stream = OpenAIStream(completion);
     // return completion.choices[0].message.content;
